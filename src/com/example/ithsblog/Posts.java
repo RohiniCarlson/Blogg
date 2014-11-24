da40a29d7daea966bdc93b7295162a5e777569a7
C.BARWICH <it-hogskolan@c.lan>
2014-11-24 15:14:51 +0100

    Camera

--- src/com/example/ithsblog/Posts.java
+++ src/com/example/ithsblog/Posts.java
@@ -1,41 +1,54 @@
 package com.example.ithsblog;
+
+
 import android.support.v7.app.ActionBarActivity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 
 
 
 public class Posts extends ActionBarActivity {
+	private static final int CAMERA_REQUEST = 1888; 
+    private ImageView imageView;
 	private static final int CAPTURE_IMAGE_CAPTURE_CODE = 0;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_posts);
-		Intent i;
-		i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-		startActivityForResult(i, CAPTURE_IMAGE_CAPTURE_CODE);
+		
+		 this.imageView = (ImageView)this.findViewById(R.id.image_view);
+	        Button photoButton = (Button) this.findViewById(R.id.upload_button);
+	        photoButton.setOnClickListener(new View.OnClickListener() {
 
-	}
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		//check which intent has sent back data
-		   if (requestCode == CAPTURE_IMAGE_CAPTURE_CODE) {
-		//check whether the process was success
-		      if (resultCode == RESULT_OK) {
-		         //perform your own functionality, I have displayed a Toast indication success
-		         Toast.makeText(this, "Image Captured", Toast.LENGTH_LONG).show();
-		      } 
-		//Check if the capture failed and perform your functionality
-		      else if (resultCode == RESULT_CANCELED) {
-		         Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+	            @Override
+	            public void onClick(View v) {
+	                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE); 
+	                startActivityForResult(cameraIntent, CAMERA_REQUEST); 
+	            }
+	        });
+	    }
+
+	    protected void onActivityResult(int requestCode, int resultCode, Intent data) {  
+	        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {  
+	        	Toast.makeText(this, "Image Captured", Toast.LENGTH_LONG).show();
+
+	            Bitmap photo = (Bitmap) data.getExtras().get("data"); 
+	            imageView.setImageBitmap(photo);
+
+	        } 
+	        else if (resultCode == RESULT_CANCELED) {
+		         Toast.makeText(this, "You are booring!", Toast.LENGTH_LONG).show();
 		      }
-		  }
+	     
 		
 		Button cam = (Button) findViewById(R.id.upload_button);
 
