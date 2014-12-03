d7ecbaf76f9f05a0d84fad56150ee4be4f1f63bd
ITHS <iths@JONAS-THE-MAN.local>
2014-12-03 14:45:43 +0100

    mindre ändringar

--- src/com/example/ithsblog/MainActivity.java
+++ src/com/example/ithsblog/MainActivity.java
@@ -51,6 +51,7 @@ public class MainActivity extends ActionBarActivity implements PropertyChangeLis
 		return super.onOptionsItemSelected(item);
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public void propertyChange(PropertyChangeEvent event) {
 
@@ -65,6 +66,7 @@ public class MainActivity extends ActionBarActivity implements PropertyChangeLis
 				// send to reader activity
 				Intent myTriggerActivityIntent=new Intent(this,PostList.class);
 				startActivity(myTriggerActivityIntent);
+				finish();
 			}			
 		} else if (event.getPropertyName().equals("getObjectsDone")) {
 			// log all object info, listarray 
