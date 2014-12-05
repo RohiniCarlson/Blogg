f46b5d591ce38cd191a5c3d5fac2835de3c0a355
Rohini Carlson <it-hogskolan@it-hogskolans-MacBook-Pro-96.local>
2014-12-05 21:00:38 +0100

    Modified create_new_user.php by adding mysql_real_escape_string() to protect db from MySQL injection when creating new records.

--- grupp5_documents/Rohini/create_new_user.php
+++ grupp5_documents/Rohini/create_new_user.php
@@ -4,19 +4,24 @@
 
 <?php
    
-	$name = $_POST['name'];
-	$mail = $_POST['email'];	
-	$password = $_POST['password'];
+	$name = mysql_real_escape_string($_POST['name']);
+	$mail = mysql_real_escape_string($_POST['email']);	
+	$password = mysql_real_escape_string($_POST['password']);
 
 	$result = mysqli_query($con,"SELECT 1 FROM iths_users WHERE mail = '$mail' AND password = '$password'");
 	
 	$count = mysqli_num_rows($result); 
     
-    if($count == 0){
-   		mysqli_query($con,"INSERT INTO iths_users (name,readerOrAdmin,mail,password) 
+    if($count == 0){  // User does not exist. Create new user.
+   		$add = mysqli_query($con,"INSERT INTO iths_users (name,readerOrAdmin,mail,password) 
 		VALUES ('$name',0,'$mail','$password')");
-        echo "1"; //User does not exist. Create new user.
-   } else {
+		if($add) {
+			echo "1"; // User was added to the database.
+		} else {
+			echo "-1"; // User could not be added to the database.
+		}
+        
+   } else { // User exists.
    	echo "0";		
    }
        
