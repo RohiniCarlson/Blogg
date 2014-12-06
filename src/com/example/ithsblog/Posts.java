8f331b54c95a2c6f0cf960f028bd50ca718ab975
Jonas Ekström <jonas@jonasekstrom.se>
2014-12-06 22:10:42 +0100

    ändringar i Posts, har gjort en kommentar där det kraschar

--- src/com/example/ithsblog/Posts.java
+++ src/com/example/ithsblog/Posts.java
@@ -20,99 +20,99 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 public class Posts extends ActionBarActivity {
-private static final int REQUEST_CAMERA_CODE = 10;
-private OnClickListener buttonListener = new OnClickListener(){
+	private static final int REQUEST_CAMERA_CODE = 10;
+	private OnClickListener buttonListener = new OnClickListener(){
+
+		@Override
+		public void onClick(View v) {
+
+			showCamera();
+
+		}
+
+	};
+	private ImageView imageView;
+	private Button button;
+	private Uri fileUri;
 
 	@Override
-	public void onClick(View v) {
-		
-		showCamera();
-	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_posts);
+		imageView = (ImageView) findViewById(R.id.image_view);
+		button = (Button) findViewById(R.id.upload_button);
+		button.setOnClickListener(buttonListener);
 	}
-	
-};
-private ImageView imageView;
-private Button button;
-private Uri fileUri;
-
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_posts);
-    imageView = (ImageView) findViewById(R.id.image_view);
-    button = (Button) findViewById(R.id.upload_button);
-    button.setOnClickListener(buttonListener);
-}
 
 
 
-private void showCamera(){
-//	Show camera here
-	
-	Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-	i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-	File photo = createFile();
-	 Uri.fromFile(photo);
-	i.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
-	if(i.resolveActivity(getPackageManager())!=null){
-		startActivityForResult(i, REQUEST_CAMERA_CODE);
-		
-	}
-}
+	private void showCamera(){
+		//	Show camera here
 
-private File createFile(){
-	return new File(createDirectory(),"myFile.jpg");
-}
-private File createDirectory(){
-File picDir =	Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+		Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		File photo = createFile();
+		Uri.fromFile(photo);
+		i.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+		if(i.resolveActivity(getPackageManager())!=null){
+			startActivityForResult(i, REQUEST_CAMERA_CODE);
 
-File bloggDir = new File(picDir, "bloggDir");
-
-if(!bloggDir.exists()){
-	if(!bloggDir.mkdirs()){
-		Log.d("Check", "sry kunde inte skapa folder");
-		
-	}
-	
-}
-return bloggDir;
-}
-
-@Override
-public void onActivityResult(int requestCode, int resultCode, Intent data){
-	
-	
-	if(requestCode == REQUEST_CAMERA_CODE && resultCode == RESULT_OK){
-		Toast.makeText(this, "Image Captured", Toast.LENGTH_LONG).show();
-		
-
-			Bitmap bild = (Bitmap) BitmapFactory.decodeFile(fileUri.getPath());
-			imageView.setImageBitmap(bild);
 		}
-	else if (resultCode == RESULT_CANCELED) {
-         Toast.makeText(this, "You are booring!", Toast.LENGTH_LONG).show();
-      }
-		
+	}
+
+	private File createFile(){
+		return new File(createDirectory(),"myFile.jpg");
+	}
+	private File createDirectory(){
+		File picDir =	Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+
+		File bloggDir = new File(picDir, "bloggDir");
+
+		if(!bloggDir.exists()){
+			if(!bloggDir.mkdirs()){
+				Log.d("Check", "sry kunde inte skapa folder");
+
+			}
+
+		}
+		return bloggDir;
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data){
+
+
+		if(requestCode == REQUEST_CAMERA_CODE && resultCode == RESULT_OK){
+			Toast.makeText(this, "Image Captured", Toast.LENGTH_LONG).show();
+
+			// HÄR KRASCHAR DET
+			// Bitmap bild = (Bitmap) BitmapFactory.decodeFile(fileUri.getPath());
+			// imageView.setImageBitmap(bild);
+		}
+		else if (resultCode == RESULT_CANCELED) {
+			Toast.makeText(this, "You are booring!", Toast.LENGTH_LONG).show();
+		}
+
 	}
 
 
 
-@Override
-public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.main, menu);
-    return true;
-}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-@Override
-public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-    if (id == R.id.action_settings) {
-        return true;
-    }
-    return super.onOptionsItemSelected(item);
-}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
