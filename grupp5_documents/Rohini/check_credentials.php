92e2ddcdf202c6b884d55c4b81826354836f4773
Jonas Ekström <jonas@jonasekstrom.se>
2014-12-07 15:07:21 +0100

    ny fil i rohinis mapp, php

--- grupp5_documents/Rohini/check_credentials.php
+++ grupp5_documents/Rohini/check_credentials.php
@@ -7,15 +7,23 @@
 	$mail = $_GET['mail'];	
 	$password = $_GET['password'];
 
-	$result = mysqli_query($con,"SELECT status FROM iths_users WHERE mail = '$mail' AND password = '$password'");
-	
+
+
+	// $result = mysqli_query($con,"SELECT status FROM iths_users WHERE mail = '$mail' AND password = '$password'");
+
+  $sql="SELECT status FROM iths_users WHERE mail = '$mail' AND password = '$password'";
+	$result=mysqli_query($con,$sql);
 	$count = mysqli_num_rows($result);		
     
     if($count == 0){
         echo "0"; // Incorrect credentials. No matching row.
    } else {
-   	 	$row = mysqli_fetch_row($result); 
-      if (row[0] == 0) {
+   	 	
+      while($row = mysqli_fetch_assoc($result)) {
+          $status = $row["status"];
+      }
+      
+      if ($status == 0) {
         echo "2"; // Correct credentials, but registration incomplete (status=0, i.e pending).
       } else {
           echo "1";  // Correct credentials and registration complete (status=1, i.e confirmed).
