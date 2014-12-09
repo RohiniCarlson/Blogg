4d5305f317c1de120a5ff5e1295dbbb72538764c
Rohini Carlson <it-hogskolan@it-hogskolans-MacBook-Pro-96.local>
2014-12-09 18:50:22 +0100

    Testing create_new_user.php

--- grupp5_documents/Rohini/create_new_user.php
+++ grupp5_documents/Rohini/create_new_user.php
@@ -27,7 +27,34 @@
 				echo "RegistrationPending";
 				// Resend registration confirmation email?? create a resend verification script??
 			} else {
-				echo "Can be registered";
+				$verificationCode = md5(uniqid("yourveryownuniqueverificationcodefortheithsbloggapp")); // generate verification code, acts as 'key'.
+
+				$confirmationLink = "http://jonasekstrom.se/ANNAT/iths_blog/confirm_registration.php?code=" . $verificationCode;
+
+				$htmlStr = "";
+                $htmlStr .= "Hi " . $mail . ",<br /><br />";
+                 
+                $htmlStr .= "Please click the button below to confirm your registration and be able to write comments.<br /><br /><br />";
+                $htmlStr .= "<a href='{$confirmationLink}' target='_blank' style='padding:1em; font-weight:bold; background-color:blue; color:#fff;'>CONFIRM REGISTRATION</a><br /><br /><br />";
+                 
+                $htmlStr .= "Kind regards,<br />";
+                $htmlStr .= "<a href='http://jonasekstrom.se/' target='_blank'>ITHS Blogg</a><br />";
+                
+                $body = $htmlStr; 
+         
+                $name_sender = "ITHS Blogg";
+                $email_sender = "no-reply@jonasekstrom.se";
+                $subject = "Registration Confirmation | ITHS Blogg ";
+                $recipient_email = $mail;
+
+                $headers  = "MIME-Version: 1.0rn";
+                $headers .= "Content-type: text/html; charset=iso-8859-1rn";
+                $headers .= "From: {$name_sender} <{$email_sender}> n";
+                if (mail($recipient_email,$subject,$body,$headers)) {
+                	echo "MailSent";
+                } else {
+                	echo "MailUnsent";
+                }
 			}
 		}
 	}
