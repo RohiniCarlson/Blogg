c25105ca8cc4899206235bfcca80f173a67d6003
Rohini Carlson <it-hogskolan@it-hogskolans-MacBook-Pro-96.local>
2014-12-10 21:00:16 +0100

    Updated confirm_registration.php

--- grupp5_documents/Rohini/confirm_registration.php
+++ grupp5_documents/Rohini/confirm_registration.php
@@ -7,13 +7,22 @@
 	$code = mysqli_real_escape_string($con, $_GET['code']);
 	echo "<div>Your confirmation code is " . $code . "</div>";
 
-	// $result = mysqli_query($con,"SELECT 1 FROM iths_users WHERE confirm_code = '$code'");
+	$result = mysqli_query($con,"SELECT 1 FROM iths_users WHERE confirm_code = '$code'");
 	
-	// $count = mysqli_num_rows($result); 
+	$count = mysqli_num_rows($result); 
     
- //    if($count > 0){  // Record exists. Update status to confirmed.
- //   		$result = mysqli_query($con,"UPDATE iths_users SET status=1, confirm_code=null WHERE confirm_code='$code'"); 
-	// 	if($result === TRUE) {
+  	if($count > 0){  // Record exists. Update status to confirmed.
+  		if (mysqli_query($con,"UPDATE iths_users SET status=1, confirm_code=null WHERE confirm_code='$code'")) {
+  			echo "<div>Your email is valid, thanks! You may now login.</div>"; // User's status confirmed.	
+  		} else { // Could not be updated.
+  			echo "<div>Our aplologies! Your registration could not be confirmed at this time. Please try again later.</div>"; // User's satus could not be updated.
+  		}
+  	} else { // No record found.
+  		echo "<div>You maybe in the wrong page.</div>";
+  	}
+ 		
+ 		//$result = mysqli_query($con,"UPDATE iths_users SET status=1, confirm_code=null WHERE confirm_code='$code'"); 
+		//if($result === TRUE) {
 	// 		echo "<div>Your email is valid, thanks!. You may now login.</div>"; // User's status confirmed.
 	// 		// Add link to app ?? Check it out!!!
 	// 	} else {
@@ -25,6 +34,6 @@
    ?>
 
 <?php
-	//mysqli_free_result($result);  
+	mysqli_free_result($result);  
 	mysqli_close($con);
 ?>
