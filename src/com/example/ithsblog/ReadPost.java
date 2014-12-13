45fd8f9eadaf655367e6d470f60ad39db9a70537
Peter Andersson <peter.d.andersson@hotmail.com>
2014-12-13 18:29:53 +0100

    log in

--- src/com/example/ithsblog/ReadPost.java
+++ src/com/example/ithsblog/ReadPost.java
@@ -34,7 +34,7 @@ import android.widget.Toast;
 
 
 public class ReadPost extends ActionBarActivity implements PropertyChangeListener {
-	
+
 	private String title;
 	private String text;
 	private String date;
@@ -49,37 +49,37 @@ public class ReadPost extends ActionBarActivity implements PropertyChangeListene
 	private ArrayAdapter<JSONObject> adapter;
 	private Button editButton, deleteButton, addButton;
 	private SharedPreferences mySettings;
-	
-	
+
+
 	private OnClickListener editButtonListener = new OnClickListener() {
 		public void onClick(View v) { 			
 			edit();
 		}
-    };
-    private OnClickListener deleteButtonListener = new OnClickListener() {
+	};
+	private OnClickListener deleteButtonListener = new OnClickListener() {
 		public void onClick(View v) { 			
 			new CheckIfAuthor(ReadPost.this).execute();
 		}
-    };
-    private OnClickListener addButtonListener = new OnClickListener() {
+	};
+	private OnClickListener addButtonListener = new OnClickListener() {
 		public void onClick(View v) { 			
 			addComment();
 		}
-    };
-    
-    private void addComment() {
+	};
+
+	private void addComment() {
 		comment = (EditText) post.findViewById(R.id.item_comment);
-        commentText = comment.getText().toString();
-        comment.setText("");
-        if(mySettings.contains("user_id")){
-        	new AddComments(this, id, commentText, mySettings.getString("user_id", "55")).execute();
-        }else{
+		commentText = comment.getText().toString();
+		comment.setText("");
+		if(mySettings.contains("user_id")){
+			new AddComments(this, id, commentText, mySettings.getString("user_id", "55")).execute();
+		}else{
 			Toast.makeText(this, "You must be logged in to post a comment", Toast.LENGTH_LONG).show();
-        }
-        
+		}
+
 	}
 
-    
+
 	private void edit() {
 		Intent intent = new Intent(ReadPost.this, Posts.class);
 		intent.putExtra("ID", id);
@@ -89,16 +89,16 @@ public class ReadPost extends ActionBarActivity implements PropertyChangeListene
 		intent.putExtra("IMAGEURL", imageURL);
 		finish();
 		startActivity(intent);
-		
+
 	}
-	
-	
+
+
 	private void delete() {
 		new DeletePost(this, id).execute();
 		Intent deleteIntent = new Intent(ReadPost.this, PostList.class);
 		deleteIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 		startActivity(deleteIntent);
-		
+
 	}
 
 	@Override
@@ -111,39 +111,39 @@ public class ReadPost extends ActionBarActivity implements PropertyChangeListene
 		text = intent.getStringExtra("TEXT");
 		id = intent.getStringExtra("ID");
 		imageURL = intent.getStringExtra("IMAGEURL");
-		
+
 		mySettings = PreferenceManager.getDefaultSharedPreferences(this);
 		new GetComments(this, id, "0").execute();
 		startListView();
 	}
-	
+
 	@Override
 	protected void onResume(){
 		super.onResume();
-		
-		
+
+
 		//new GetComments(this).execute();
-		
+
 	}
 
 	private void startListView() {
-		
+
 		adapter = new ReadPostListAdapter(ReadPost.this, list, id);
 		LayoutInflater inflater = LayoutInflater.from(this);
 		if(mySettings.getBoolean("isAdmin", false)){
-		post = inflater.inflate(R.layout.author_post, null);
-		editButton = (Button) post.findViewById(R.id.edit_button);
-        editButton.setOnClickListener(editButtonListener);
-        deleteButton = (Button) post.findViewById(R.id.delete_button);
-        deleteButton.setOnClickListener(deleteButtonListener);
-        addButton = (Button) post.findViewById(R.id.comment_button);
-        addButton.setOnClickListener(addButtonListener);
+			post = inflater.inflate(R.layout.author_post, null);
+			editButton = (Button) post.findViewById(R.id.edit_button);
+			editButton.setOnClickListener(editButtonListener);
+			deleteButton = (Button) post.findViewById(R.id.delete_button);
+			deleteButton.setOnClickListener(deleteButtonListener);
+			addButton = (Button) post.findViewById(R.id.comment_button);
+			addButton.setOnClickListener(addButtonListener);
 		}else{
-		post = inflater.inflate(R.layout.post, null);
-		addButton = (Button) post.findViewById(R.id.comment_button);
-        addButton.setOnClickListener(addButtonListener);
+			post = inflater.inflate(R.layout.post, null);
+			addButton = (Button) post.findViewById(R.id.comment_button);
+			addButton.setOnClickListener(addButtonListener);
 		}
-		
+
 		//Image
 		ImageView imageView = (ImageView)post.findViewById(R.id.item_imageView);
 		String url = "http://jonasekstrom.se/ANNAT/iths_blog/images/"+id+".jpg";
@@ -153,28 +153,32 @@ public class ReadPost extends ActionBarActivity implements PropertyChangeListene
 			new ImageLoader(imageView, url, id).execute();
 		}
 		// Title
-	    TextView titleView = (TextView)post.findViewById(R.id.item_titleView);
-	    titleView.setText(title);
-	    //Text
-	    TextView textView = (TextView)post.findViewById(R.id.item_textView);
-	    textView.setText(text);
-	    // Date
-	    TextView dateView = (TextView)post.findViewById(R.id.item_dateView);
+		TextView titleView = (TextView)post.findViewById(R.id.item_titleView);
+		titleView.setText(title);
+		//Text
+		TextView textView = (TextView)post.findViewById(R.id.item_textView);
+		textView.setText(text);
+		// Date
+		TextView dateView = (TextView)post.findViewById(R.id.item_dateView);
 		dateView.setText(date);
-		
+
 		listView = (ListView) findViewById(R.id.comment_list);
 		listView.addHeaderView(post);
 		listView.setAdapter(adapter);
-		
+
 	}
 
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.read_post, menu);
-			menu.findItem(R.id.action_new_post).setVisible(mySettings.getBoolean("author", false));
-		return true;
+		inflateMenu(menu);
+		return true;						
+	}
+
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		inflateMenu(menu);
+		return super.onPrepareOptionsMenu(menu);
 	}
 
 	@Override
@@ -185,12 +189,35 @@ public class ReadPost extends ActionBarActivity implements PropertyChangeListene
 		int id = item.getItemId();
 		if (id == R.id.action_new_post) {
 			Intent intent = new Intent (ReadPost.this , Posts.class);
-        	startActivity(intent);
-        	finish();
+			startActivity(intent);
+			finish();
+			return true;
+		}else if (id == R.id.action_login) {
+			showSignInScreen();
+			return true;
+		} else if (id == R.id.action_logout) {
+			LogOut.doLogOut(this);
+			supportInvalidateOptionsMenu();
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
+	private void inflateMenu(Menu menu) {
+		menu.clear();
+		if (mySettings.contains("sessionId") && mySettings.contains("isAdmin")) {			
+			getMenuInflater().inflate(R.menu.logout, menu);
+		} else {
+			getMenuInflater().inflate(R.menu.post_list, menu);
+		}
+		menu.findItem(R.id.action_new_post).setVisible(mySettings.getBoolean("isAdmin", false));
+	}
+
+	private void showSignInScreen() {
+		Intent intent = new Intent(ReadPost.this, LogIn.class);
+		startActivity(intent);
+	}
+
 
 	@Override
 	public void propertyChange(PropertyChangeEvent event) {
@@ -211,49 +238,49 @@ public class ReadPost extends ActionBarActivity implements PropertyChangeListene
 			new GetComments(this, id, "0").execute();
 		}
 	}
-	
+
 	// The method that displays the popup.
-		private void showPopup(final Activity context) {
-		   int popupWidth = 330;
-		   int popupHeight = 250;
-		 
-		   // Inflate the popup_layout.xml
-		   LinearLayout viewGroup = (LinearLayout) context.findViewById(R.id.popup);
-		   LayoutInflater layoutInflater = (LayoutInflater) context
-		     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		   View layout = layoutInflater.inflate(R.layout.delete_popup, viewGroup);
-		 
-		   // Creating the PopupWindow
-		   final PopupWindow popup = new PopupWindow(context);
-		   popup.setContentView(layout);
-		   popup.setWidth(popupWidth);
-		   popup.setHeight(popupHeight);
-		   popup.setFocusable(true);
-		 
+	private void showPopup(final Activity context) {
+		int popupWidth = 330;
+		int popupHeight = 250;
 
-		   // Displaying the popup at the specified location, + offsets.
-		   popup.showAtLocation(layout, Gravity.CENTER, 0, 0);
+		// Inflate the popup_layout.xml
+		LinearLayout viewGroup = (LinearLayout) context.findViewById(R.id.popup);
+		LayoutInflater layoutInflater = (LayoutInflater) context
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View layout = layoutInflater.inflate(R.layout.delete_popup, viewGroup);
 
-		 
-		   // Getting a reference to Cancle button, and close the popup when clicked.
-		   Button close = (Button) layout.findViewById(R.id.delete_cancel_button);
-		   close.setOnClickListener(new OnClickListener() {
-		 
-		     @Override
-		     public void onClick(View v) {
-		    	 popup.dismiss();
-		     }
-		   });
-		   
-		   // Getting a reference to Delete button, and close the popup when clicked.
-		   Button delete = (Button) layout.findViewById(R.id.delete_button);
-		   delete.setOnClickListener(new OnClickListener() {
-		 
-		     @Override
-		     public void onClick(View v) {
-		    	 delete();
-		    	 popup.dismiss();
-		     }
-		   });
-		}
+		// Creating the PopupWindow
+		final PopupWindow popup = new PopupWindow(context);
+		popup.setContentView(layout);
+		popup.setWidth(popupWidth);
+		popup.setHeight(popupHeight);
+		popup.setFocusable(true);
+
+
+		// Displaying the popup at the specified location, + offsets.
+		popup.showAtLocation(layout, Gravity.CENTER, 0, 0);
+
+
+		// Getting a reference to Cancle button, and close the popup when clicked.
+		Button close = (Button) layout.findViewById(R.id.delete_cancel_button);
+		close.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				popup.dismiss();
+			}
+		});
+
+		// Getting a reference to Delete button, and close the popup when clicked.
+		Button delete = (Button) layout.findViewById(R.id.delete_button);
+		delete.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				delete();
+				popup.dismiss();
+			}
+		});
+	}
 }
